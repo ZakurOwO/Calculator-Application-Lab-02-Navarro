031f69236ad7dd4d4c0a890ab037bf91d5740f41
ZakurOwO <johndanielnavarro130@gmail.com>
2025-08-13 20:11:51 +0800

    UpdateOne

--- CalculatorApplication/Form1.Designer.cs
+++ CalculatorApplication/Form1.Designer.cs
@@ -28,20 +28,149 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxInput1 = new System.Windows.Forms.TextBox();
+            this.cbOperator = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxInput2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblDisplayTotal = new System.Windows.Forms.Label();
+            this.btnEqual = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(98)))), ((int)(((byte)(106)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(241)))), ((int)(((byte)(199)))));
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter First Number:";
+            // 
+            // txtBoxInput1
+            // 
+            this.txtBoxInput1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(98)))), ((int)(((byte)(106)))));
+            this.txtBoxInput1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtBoxInput1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(241)))), ((int)(((byte)(199)))));
+            this.txtBoxInput1.Location = new System.Drawing.Point(202, 24);
+            this.txtBoxInput1.Name = "txtBoxInput1";
+            this.txtBoxInput1.Size = new System.Drawing.Size(100, 26);
+            this.txtBoxInput1.TabIndex = 1;
+            this.txtBoxInput1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // cbOperator
+            // 
+            this.cbOperator.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(98)))), ((int)(((byte)(106)))));
+            this.cbOperator.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbOperator.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(241)))), ((int)(((byte)(199)))));
+            this.cbOperator.FormattingEnabled = true;
+            this.cbOperator.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.cbOperator.Location = new System.Drawing.Point(225, 56);
+            this.cbOperator.Name = "cbOperator";
+            this.cbOperator.Size = new System.Drawing.Size(55, 27);
+            this.cbOperator.TabIndex = 2;
+            this.cbOperator.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(98)))), ((int)(((byte)(106)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(241)))), ((int)(((byte)(199)))));
+            this.label2.Location = new System.Drawing.Point(12, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter Second Number:";
+            // 
+            // txtBoxInput2
+            // 
+            this.txtBoxInput2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(98)))), ((int)(((byte)(106)))));
+            this.txtBoxInput2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtBoxInput2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(241)))), ((int)(((byte)(199)))));
+            this.txtBoxInput2.Location = new System.Drawing.Point(202, 93);
+            this.txtBoxInput2.Name = "txtBoxInput2";
+            this.txtBoxInput2.Size = new System.Drawing.Size(100, 26);
+            this.txtBoxInput2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(98)))), ((int)(((byte)(106)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(241)))), ((int)(((byte)(199)))));
+            this.label3.Location = new System.Drawing.Point(32, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Answer:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblDisplayTotal
+            // 
+            this.lblDisplayTotal.AutoSize = true;
+            this.lblDisplayTotal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(98)))), ((int)(((byte)(106)))));
+            this.lblDisplayTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblDisplayTotal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(241)))), ((int)(((byte)(199)))));
+            this.lblDisplayTotal.Location = new System.Drawing.Point(180, 153);
+            this.lblDisplayTotal.Name = "lblDisplayTotal";
+            this.lblDisplayTotal.Size = new System.Drawing.Size(51, 20);
+            this.lblDisplayTotal.TabIndex = 6;
+            this.lblDisplayTotal.Text = "label4";
+            this.lblDisplayTotal.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // btnEqual
+            // 
+            this.btnEqual.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(98)))), ((int)(((byte)(106)))));
+            this.btnEqual.Font = new System.Drawing.Font("Consolas", 14F);
+            this.btnEqual.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(241)))), ((int)(((byte)(199)))));
+            this.btnEqual.Location = new System.Drawing.Point(184, 263);
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.Size = new System.Drawing.Size(112, 30);
+            this.btnEqual.TabIndex = 7;
+            this.btnEqual.Text = "Calculate";
+            this.btnEqual.UseVisualStyleBackColor = false;
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(313, 439);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(241)))), ((int)(((byte)(199)))));
+            this.ClientSize = new System.Drawing.Size(308, 305);
+            this.Controls.Add(this.btnEqual);
+            this.Controls.Add(this.lblDisplayTotal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBoxInput2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbOperator);
+            this.Controls.Add(this.txtBoxInput1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxInput1;
+        private System.Windows.Forms.ComboBox cbOperator;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxInput2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblDisplayTotal;
+        private System.Windows.Forms.Button btnEqual;
     }
 }
 
--- CalculatorApplication/Form1.cs
+++ CalculatorApplication/Form1.cs
@@ -12,9 +12,130 @@ namespace CalculatorApplication
 {
     public partial class Form1 : Form
     {
+
+
+        CalculatorClass cal;
+
+
+        double num1;
+        double num2;
+
+        
         public Form1()
         {
             InitializeComponent();
+            cal = new CalculatorClass();
+        }
+
+
+        public delegate T Formula<T>(T arg1, T arg2);
+        public Formula<double> DoubleType;
+
+        public class CalculatorClass
+        {
+          
+
+            public double GetSum(double arg1, double arg2)
+            {
+                return arg1 + arg2;
+            }
+            public double GetDifference(double arg1, double arg2)
+            {
+                return arg1 - arg2;
+            }
+            public double GetProduct(double arg1, double arg2)
+            {
+                return arg1 * arg2;
+            }
+            public double GetQuotient(double arg1, double arg2)
+            {
+                if (arg2 == 0)
+                {
+                    throw new DivideByZeroException("Cannot divide by zero.");
+                }
+                return arg1 / arg2;
+            }
+
+
+
+
+
+        }
+
+
+        public delegate double CalculatorDelegate(double arg1, double arg2);
+
+        private CalculatorDelegate calculateEvent;
+
+        public event CalculatorDelegate CalculateEvent
+        {
+            add
+            {
+                calculateEvent += value;
+                Console.WriteLine("Added the Delegate");
+            }
+            remove
+            {
+                calculateEvent -= value;
+                Console.WriteLine("Removed the Delegate");
+            }
+        }
+
+
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToDouble(txtBoxInput1.Text);
+            num2 = Convert.ToDouble(txtBoxInput2.Text);
+
+            string op = cbOperator.SelectedItem.ToString();
+            double result = 0;
+
+            if (op == "+")
+            {
+                
+            }
+            else if (op == "-")
+            {
+                DoubleType = cal.GetDifference;
+            }
+            else if (op == "*")
+            {
+                DoubleType = cal.GetProduct;
+            }
+            else if (op == "/")
+            {
+                DoubleType = cal.GetQuotient;
+            }
+
+
         }
     }
 }
